4bb64a44a08f31f46cc05f86e86c89401b982e32
sailokesh16 <sailokesh16u@gmail.com>
2024-11-12 09:31:38 +0530

    static web page with html and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+    fontFamily:{
+      'rowdies':'"Rowdies"',
+      'righteous':'"Righteous"',
+      'Saira':'"Saira"',
+    },
+    textColor: {
+      'regal-blue': '#4883FF',
+    },
+    colors:{
+      'verylightblue' : '#D9ECFF',
+      'lightwhite':'#F0F3FF',
+      'custom-blue': '#00B3FF',
+      'custom-via': '#1901F8',
+      'custom-purple': '#E441FF',
+    },
+  },
+  },
+  plugins: [],
+}
+
